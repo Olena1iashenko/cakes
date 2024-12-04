df73b7280dd14361419e135bebfe2e35d21fa885
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2024-12-04 17:32:39 +0200

    /

--- src/assets/cakes.js
+++ src/assets/cakes.js
@@ -6,8 +6,8 @@ const cakes = [
     productName: "Pumpkin Latte Cake",
     price: 100,
     images: [
-      "../../public/images/pumpkin-late-1.jpeg",
-      "../../public/images/pumpkin-late-2.jpeg",
+      "../../images/pumpkin-late-1.jpeg",
+      "../../images/pumpkin-late-2.jpeg",
     ],
   },
   {
@@ -15,8 +15,8 @@ const cakes = [
     productName: "Chocolate Cherry Cake",
     price: 120,
     images: [
-      "../../public/images/choco-cherry-1.jpeg",
-      "../../public/images/choco-cherry-2.jpeg",
+      "../../images/choco-cherry-1.jpeg",
+      "../../images/choco-cherry-2.jpeg",
     ],
   },
   {
@@ -24,35 +24,29 @@ const cakes = [
     productName: "Red Velvet Cranberry Cake",
     price: 130,
     images: [
-      "../../public/images/red-velvet-1.jpeg",
-      "../../public/images/red-velvet-2.jpeg",
+      "../../images/red-velvet-1.jpeg",
+      "../../images/red-velvet-2.jpeg",
     ],
   },
   {
     id: nanoid(),
     productName: "Cupcakes",
     price: 100,
-    images: [
-      "../../public/images/cupcakes-1.jpeg",
-      "../../public/images/cupcakes-2.jpeg",
-    ],
+    images: ["../../images/cupcakes-1.jpeg", "../../images/cupcakes-2.jpeg"],
   },
   {
     id: nanoid(),
     productName: "Strudel",
     price: 90,
-    images: [
-      "../../public/images/strudel.jpeg",
-      "../../public/images/strudel.jpeg",
-    ],
+    images: ["../../images/strudel.jpeg", "../../images/strudel.jpeg"],
   },
   {
     id: nanoid(),
     productName: "Poppy Cherry Cake",
     price: 120,
     images: [
-      "../../public/images/poppy-cherry.jpeg",
-      "../../public/images/poppy-cherry.jpeg",
+      "../../images/poppy-cherry.jpeg",
+      "../../images/poppy-cherry.jpeg",
     ],
   },
   {
@@ -60,36 +54,30 @@ const cakes = [
     productName: "Lemon cupcakes",
     price: 90,
     images: [
-      "../../public/images/lemon-cupcakes.jpeg",
-      "../../public/images/lemon-cupcakes.jpeg",
+      "../../images/lemon-cupcakes.jpeg",
+      "../../images/lemon-cupcakes.jpeg",
     ],
   },
   {
     id: nanoid(),
     productName: "Brownie",
     price: 190,
-    images: [
-      "../../public/images/brownie.jpeg",
-      "../../public/images/brownie.jpeg",
-    ],
+    images: ["../../images/brownie.jpeg", "../../images/brownie.jpeg"],
   },
   {
     id: nanoid(),
     productName: "Blueberry Tart",
     price: 190,
     images: [
-      "../../public/images/blueberry-tarte.jpeg",
-      "../../public/images/blueberry-tarte.jpeg",
+      "../../images/blueberry-tarte.jpeg",
+      "../../images/blueberry-tarte.jpeg",
     ],
   },
   {
     id: nanoid(),
     productName: "Basques Cheesecake",
     price: 190,
-    images: [
-      "../../public/images/basques.jpeg",
-      "../../public/images/basques.jpeg",
-    ],
+    images: ["../../images/basques.jpeg", "../../images/basques.jpeg"],
   },
 ];
 
